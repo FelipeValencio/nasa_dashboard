d0edb83afc40f3ea216b44f825ac2755c0b8af68
Felipe Gomes Valencio <felipe.valencio@hotmail.com>
2020-05-31 00:37:28 -0300

    bug fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,90 +28,55 @@ export class HomeComponent implements OnInit {
     this.httpService.sendGetRequest().subscribe(res => this.processData(res));
   }
 
-  public series: any[];
-
   public options: any = {
 
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Monthly Average Temperature'
+    },
+    xAxis: {
+      categories: []
+    },
+    yAxis: {
       title: {
-          text: 'Solar Employment Growth by Sector, 2010-2016'
-      },
-      subtitle: {
-          text: 'Source: thesolarfoundation.com'
-      },
-      yAxis: {
-          title: {
-              text: 'Number of Employees'
-          }
-      },
-      xAxis: {
-          accessibility: {
-              rangeDescription: 'Range: 2010 to 2017'
-          }
-      },
-      legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle'
-      },
-
-      plotOptions: {
-          series: {
-              label: {
-                  connectorAllowed: false
-              },
-              pointStart: 2010
-          }
-      },
-
-      series: [{
-          name: 'Installation',
-          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-      }, {
-          name: 'Manufacturing',
-          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-      }, {
-          name: 'Sales & Distribution',
-          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-      }, {
-          name: 'Project Development',
-          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-      }, {
-          name: 'Other',
-          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-      }],
-
-      responsive: {
-          rules: [{
-              condition: {
-                  maxWidth: 500
-              },
-              chartOptions: {
-                  legend: {
-                      layout: 'horizontal',
-                      align: 'center',
-                      verticalAlign: 'bottom'
-                  }
-              }
-          }]
+        text: 'Contaminado sla'
       }
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true
+        },
+        enableMouseTracking: false
+      }
+    },
+    series: [],
   }
 
   processData(res) {
 
-    /* for(let r of res) {
-      this.series.push({
-        name: r.Country,
-        data: null
+    let data = [];
+
+    for(let r of res) {
+      r.confirmed = r.confirmed.map(Number);
+      data.push({
+        name: r.country,
+        data: r.confirmed
       });
-    } */
-    this.buildChart(res);
+    }
+    
+
+    this.buildChart(data, res[0].dates);
   }
 
-  buildChart(res) {
+  buildChart(data, dates) {
 
-    console.log(res)
+    this.options.xAxis.categories = dates;
+    this.options.series = data;
 
-    this.options.series = res;
+    console.log(data)
 
     Highcharts.chart('container', this.options);
   }
